bd51a8038402a316a4b117bd87b738b112d6a29f
shanzi <yun.er.run@gmail.com>
2013-04-23 00:09:25 +0800

    generic searcher

--- src/qur/__init__.py
+++ src/qur/__init__.py
@@ -9,11 +9,12 @@
 
 
 import math,re,ignorewords
+from bson.code import Code
 
 __VERSION__ = (0,1)
 __EN_WORD_CUT__ = re.compile(r"\W*")
-__CN_WORD_CUT__ = re.compile(ur"[^\u4E00-\u9FA5a-zA-Z0-9+#]*")
 __WORD_MIN_SCORE__ = 0.001
+__SEARCH_WORDS_LIMIT__ = 3
 
 
 def versionstring():
@@ -29,17 +30,13 @@ class DBStruct:
     def ensureIndex(self):
         self.words.ensure_index({"word":1,"freq":1})
         self.relations.ensure_index({"word":1,"score":-1})
-        self.relations.ensure_index({"entry_id":1})
+        self.relations.ensure_index({"entry_id":1,"score":-1})
 
 class GenericIndexer(DBStruct):
-    def __init__(self, db, name="",jieba_word_cut=False):
+    def __init__(self, db, name="",word_cut=False):
         super(DBStruct, self).__init__(db,name)
 
-        if jieba_word_cut:
-            import jieba
-            self.jieba = jieba
-        else:
-            self.jieba = None
+        self.word_cut = word_cut
 
     def indexText(self,entry_id,text):
         words = self.seperateWords(text)
@@ -58,11 +55,11 @@ class GenericIndexer(DBStruct):
 
     def seperateWords(self,text):
         text = test.lower().strip()
-        if self.jieba:
+        if self.word_cut:
             sentences=__EN_WORD_CUT__.split(unicode(text))
             words=[]
             for s in sentences:
-                for w in jieba.cut(s):
+                for w in word_cut.cut(s):
                     words.append(w)
             return words
         else:
@@ -72,11 +69,11 @@ class GenericIndexer(DBStruct):
     def calculateWordScore(self,words):
         wordsdict = {}
         for w in words:
-            if len(w) > 20:
+            if w and len(w) > 20 :
                 continue
             elif w in ignorewords.EN:
                 continue
-            elif self.jieba and w in ignorewords.CN:
+            elif self.word_cut and w in ignorewords.CN:
                 continue
             elif wordsdict.get(w):
                 wordsdict[w]+=1
@@ -95,5 +92,29 @@ class GenericIndexer(DBStruct):
 
 class GenericSearcher(DBStruct):
     def __init__(self,db,name):
-        super(DBStruct, self).__init__(db,name)
-        
+        super(DBStruct, self).__init__(db,name,word_cut)
+        self.word_cut = word_cut
+
+    def processSearchString(self,searchstring):
+        if word_cut:
+            words = word_cut.cut(searchstring)
+        else:
+            words = __EN_WORD_CUT__.split(searchstring)
+        ws    = self.words.find({"word":{"$in":words}}).sort({"freq":1})
+        query = [ws.next()["word"] for x in range(__SEARCH_WORDS_LIMIT__)]
+        ret   = this.relations.aggregate(
+                {"$match":{"word":{"$in":query}}},
+                {"$group":{
+                    "_id":{"entry_id":"$entry_id"},
+                    "score":{"$sum":"$score"},
+                    "matched_words":{"$addToSet":"$word"}
+                    }},
+                {"$limit":100},
+                {"$sort":{"score":-1}});
+        if ret[ok]:
+            return ret["result"]
+        else:
+            return None
+            
+
+
